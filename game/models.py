30756afa109db31fd9387dc2e1617f579133503c
Augusto_Mancuso <a.mancuso@alumno.um.edu.ar>
2023-08-26 12:15:42 -0300

    Print board implemeted and tested.
    Looking fordward creating remaining classes.

--- game/models.py
+++ game/models.py
@@ -80,27 +80,50 @@ class TilesBag:
 
 class Board():
     # La secuencia seria imprimir el tablero luego de haber hecho todas las acciones
+
+    def add_tile(self, tile, row, column):
+        if isinstance(self.board[row][column].letter, Tile):
+            return "Celda ocupada"
+        else:
+            self.board[row][column].letter = tile
+
     def __init__(self):
         self.board = [[Cell(1, "") for _ in range(15)] for _ in range(15)]
         init(autoreset=True)
         for tipo, valores in coordenadas_multiplicadores.items():
             for i, j in valores:
                 if i == 7 and j == 7:
-                    continue
+                    self.board[i][j].add_letter = Tile("*", 0)
                 self.board[i][j] = Cell(multiplicadores_valores[tipo], tipo.split("_")[1])
-        self.board[7][7] = Cell(1, "word")
 
     def get_tile(self, row, column):
         return self.board[row][column]
-
-    def add_tile(self, tile, row, column):
-        if isinstance(self.board[row][column], Tile):
-            return "Celda ocupada"
-        else:
-            self.board[row][column] = tile
 
     def remove_tile(self, row, column):
         if (self.board[row][column].letter is not None):
             self.board[row][column].letter = None
         else:
             return "No hay ficha en la celda"
+
+    # El método funciona, pero no encuentro forma de testearlo correctamente.
+    def print_board(self):
+        print(Fore.GREEN + "+---------" * 15 + "+")
+        for row in self.board:
+            formatted_row = []
+            for cell in row:
+                if isinstance(cell.letter, Tile):
+                    # Si hay una letra asignada a la celda
+                    if cell.letter is not None:
+                        if len(cell.letter.letter) == 1:
+                            formatted_row.append(f"{Fore.GREEN}|   {Fore.YELLOW}{cell.letter.letter}{cell.letter.value:2d}   ")
+                        else:
+                            formatted_row.append(f"{Fore.GREEN}|  {Fore.YELLOW}{cell.letter.letter}{cell.letter.value:2d}   ")
+                # Si hay multiplicadores
+                elif cell.multiplier != 1:
+                    formatted_row.append(f"{Fore.CYAN}| x{(cell.multiplier)}{(cell.multiplier_type):^6}")
+                else:
+                    formatted_row.append(f"{Fore.GREEN}|{Fore.WHITE:^14}")
+            formatted_row.append(Fore.GREEN + "|")
+            print("".join(formatted_row))
+            print(Fore.GREEN + "+---------" * 15 + "+")
+
--- test/test_models.py
+++ test/test_models.py
@@ -1,6 +1,9 @@
 import unittest
 from unittest.mock import patch, Mock
 from game.models import *
+from io import StringIO
+import sys
+import re
 import pdb
 
 
@@ -76,18 +79,18 @@ class TestBoard(unittest.TestCase):
         self.assertIsInstance(tile, Cell)
 
     def test_add_tile_empty_cell(self):
-        tile = Tile("A", 1)  # Suponiendo que tienes una clase Tile
+        tile = Tile("A", 1)
         result = self.board.add_tile(tile, 0, 0)
         self.assertIsNone(result)
 
     def test_add_tile_occupied_cell(self):
-        tile = Tile("B", 2)  # Suponiendo que tienes una clase Tile
+        tile = Tile("B", 2)
         self.board.add_tile(tile, 0, 0)
         result = self.board.add_tile(tile, 0, 0)
         self.assertEqual(result, "Celda ocupada")
 
     def test_remove_tile_with_letter(self):
-        tile = Tile("C", 3)  # Suponiendo que tienes una clase Tile
+        tile = Tile("C", 3)
         self.board.add_tile(tile, 0, 0)
         self.board.remove_tile(0, 0)
         self.assertIsNone(self.board.get_tile(0, 0).letter)
@@ -95,6 +98,103 @@ class TestBoard(unittest.TestCase):
     def test_remove_tile_empty_cell(self):
         result = self.board.remove_tile(3, 0)
         self.assertEqual(result, "No hay ficha en la celda")
+
+
+class TestPrintBoard(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        self.board = Board()
+        self.expected_output = [
+                "+---------" * 15 + "+",
+                "| x3 word |         |         | x2letter|         |         |         | x3 word |         |         |         | x2letter|         |         | x3 word |",
+                "+---------" * 15 + "+",
+                "|         | x2 word |         |         |         | x3letter|         |         |         | x3letter|         |         |         | x2 word |         |",
+                "+---------" * 15 + "+",
+                "|         |         | x2 word |         |         |         | x2letter|         | x2letter|         |         |         | x2 word |         |         |",
+                "+---------" * 15 + "+",
+                "| x2letter|         |         | x2 word |         |         |         | x2letter|         |         |         | x2 word |         |         | x2letter|",
+                "+---------" * 15 + "+",
+                "|         |         |         |         | x2 word |         |         |         |         |         | x2 word |         |         |         |         |",
+                "+---------" * 15 + "+",
+                "|         | x3letter|         |         |         | x3letter|         |         |         | x3letter|         |         |         | x3letter|         |",
+                "+---------" * 15 + "+",
+                "|         |         | x2letter|         |         |         | x2letter|         | x2letter|         |         |         | x2letter|         |         |",
+                "+---------" * 15 + "+",
+                "| x3 word |         |         | x2letter|         |         |         | x2 word |         |         |         | x2letter|         |         | x3 word |",
+                "+---------" * 15 + "+",
+                "|         |         | x2letter|         |         |         | x2letter|         | x2letter|         |         |         | x2letter|         |         |",
+                "+---------" * 15 + "+",
+                "|         | x3letter|         |         |         | x3letter|         |         |         | x3letter|         |         |         | x3letter|         |",
+                "+---------" * 15 + "+",
+                "|         |         |         |         | x2 word |         |         |         |         |         | x2 word |         |         |         |         |",
+                "+---------" * 15 + "+",
+                "| x2letter|         |         | x2 word |         |         |         | x2letter|         |         |         | x2 word |         |         | x2letter|",
+                "+---------" * 15 + "+",
+                "|         |         | x2 word |         |         |         | x2letter|         | x2letter|         |         |         | x2 word |         |         |",
+                "+---------" * 15 + "+",
+                "|         | x2 word |         |         |         | x3letter|         |         |         | x3letter|         |         |         | x2 word |         |",
+                "+---------" * 15 + "+",
+                "| x3 word |         |         | x2letter|         |         |         | x3 word |         |         |         | x2letter|         |         | x3 word |",
+                "+---------" * 15 + "+"
+            ]
+        self.expected_output_1_tile = self.expected_output.copy()
+        self.expected_output_2_tile = self.expected_output.copy()
+        self.expected_output_1_tile[1] = "|   A 1   |         |         | x2letter|         |         |         | x3 word |         |         |         | x2letter|         |         | x3 word |"
+        self.expected_output_2_tile[1] = "|   A 1   |  RR 1   |         | x2letter|         |         |         | x3 word |         |         |         | x2letter|         |         | x3 word |"
+
+    def remove_color_codes(self, text):
+        color_pattern = re.compile(r'\x1b\[[0-9;]+m')
+        return color_pattern.sub('', text)
+
+    def test_print_board_with_empty_cells(self):
+
+        with StringIO() as mock_stdout:
+            sys.stdout = mock_stdout
+            self.board.print_board()
+            sys.stdout = sys.__stdout__
+
+            output_lines = mock_stdout.getvalue().strip().split("\n")
+
+        output_lines = [self.remove_color_codes(line) for line in output_lines]
+
+        captured_lines = []
+        for expected_line, actual_line in zip(self.expected_output, output_lines):
+            self.assertEqual(expected_line, actual_line)
+            captured_lines.append(actual_line)
+
+    def test_print_board_1_tile(self):
+
+        with StringIO() as mock_stdout:
+            sys.stdout = mock_stdout
+            self.board.add_tile(Tile("A", 1), 0, 0)
+            self.board.print_board()
+            sys.stdout = sys.__stdout__
+
+            output_lines = mock_stdout.getvalue().strip().split("\n")
+
+        output_lines = [self.remove_color_codes(line) for line in output_lines]
+
+        captured_lines = []
+        for expected_line, actual_line in zip(self.expected_output_1_tile, output_lines):
+            self.assertEqual(expected_line, actual_line)
+            captured_lines.append(actual_line)
+
+    def test_print_board_2_tiles(self):
+
+        with StringIO() as mock_stdout:
+            sys.stdout = mock_stdout
+            self.board.add_tile(Tile("A", 1), 0, 0)
+            self.board.add_tile(Tile("RR", 1), 0, 1)
+            self.board.print_board()
+            sys.stdout = sys.__stdout__
+            output_lines = mock_stdout.getvalue().strip().split("\n")
+
+        output_lines = [self.remove_color_codes(line) for line in output_lines]
+        captured_lines = []
+        for expected_line, actual_line in zip(self.expected_output_2_tile, output_lines):
+            self.assertEqual(expected_line, actual_line)
+            captured_lines.append(actual_line)
 
 
 class TestCells(unittest.TestCase):
@@ -124,6 +224,7 @@ class TestCells(unittest.TestCase):
         letter = Tile(letter='p', value=3)
         cell.add_letter(letter=letter)
         self.assertEqual(cell.calculate_value(),3)
+
 
 if __name__ == '__main__':
 
