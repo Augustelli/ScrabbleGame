70d1e8847980d608a394ff5f6adcb971d9ff3461
Augusto_Mancuso <a.mancuso@alumno.um.edu.ar>
2023-11-01 04:03:52 -0300

    Test Cc 20

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# Ejecuta las pruebas
 coverage run -m unittest
+
+# Obtiene el código de retorno de la última ejecución
+return_code=$?
+
+# Genera el informe de cobertura
 coverage report -m
-exit ${PIPESTATUS[0]}
+
+# Si el código de retorno es 0 (éxito), salimos con 0, de lo contrario, salimos con el código de retorno de las pruebas
+if [ $return_code -eq 0 ]; then
+  exit 0
+else
+  exit $return_code
+fi
